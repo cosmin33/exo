b45237f68d99a18d3fbf4c80109a6c9400a88d37
cosmin33 <3cosmo@gmail.com>
2023-07-07 15:09:37 +0300

    some cosmethics

--- src/main/scala/io/cosmo/exo/internal/FunctionHKFunctions.scala
+++ src/main/scala/io/cosmo/exo/internal/FunctionHKFunctions.scala
@@ -7,48 +7,48 @@ import io.cosmo.exo.syntax._
 import io.cosmo.exo.internal.any._
 
 trait FunctionHKFunctions {
-  def id[F[_[_]]]: F ≈> F = ∀~.mk[F ≈> F].from(identity)
-  def andThen[F[_[_]], G[_[_]], H[_[_]]](fg: F ≈> G, gh: G ≈> H): F ≈> H = ∀~.mk[F ≈> H].from(fg.apply andThen gh.apply)
-  def initiate[F[_[_]]]: VoidHK ≈> F = ∀~.mk[VoidHK ≈> F].from(identity)
-  def terminate[F[_[_]]]: F ≈> UnitHK = ∀~.mk[F ≈> UnitHK].from(_ => ())
-  def distribute[F[_[_]], G[_[_]], H[_[_]]](fg: F ≈> G, fh: F ≈> H): F ≈> ([a[_]] =>> (G[a], H[a])) =
-    ∀~.mk[F ≈> ([a[_]] =>> (G[a], H[a]))].from(fa => (fg.apply(fa), fh.apply(fa)))
+  def id[A[_[_]]]: A ≈> A = ∀~.mk[A ≈> A].from(identity)
+  def andThen[A[_[_]], B[_[_]], H[_[_]]](fg: A ≈> B, gh: B ≈> H): A ≈> H = ∀~.mk[A ≈> H].from(fg.apply andThen gh.apply)
+  def initiate[A[_[_]]]: VoidHK ≈> A = ∀~.mk[VoidHK ≈> A].from(identity)
+  def terminate[A[_[_]]]: A ≈> UnitHK = ∀~.mk[A ≈> UnitHK].from(_ => ())
+  def distribute[A[_[_]], B[_[_]], H[_[_]]](fg: A ≈> B, fh: A ≈> H): A ≈> ([f[_]] =>> (B[f], H[f])) =
+    ∀~.mk[A ≈> ([f[_]] =>> (B[f], H[f]))].from(fa => (fg.apply(fa), fh.apply(fa)))
   object product {
-    def associate[F[_[_]], G[_[_]], H[_[_]]]: ([a[_]] =>> ((F[a], G[a]), H[a])) ≈> ([a[_]] =>> (F[a], (G[a], H[a]))) =
-      ∀~.mk[([a[_]] =>> ((F[a], G[a]), H[a])) ≈> ([a[_]] =>> (F[a], (G[a], H[a])))].from(Associative[* => *, (*, *)].associate)
-    def diassociate[F[_[_]], G[_[_]], H[_[_]]]: ([a[_]] =>> (F[a], (G[a], H[a]))) ≈> ([a[_]] =>> ((F[a], G[a]), H[a])) =
-      ∀~.mk[([a[_]] =>> (F[a], (G[a], H[a]))) ≈> ([a[_]] =>> ((F[a], G[a]), H[a]))].from(Associative[* => *, (*, *)].diassociate)
-    def bimap[F[_[_]], G[_[_]], X[_[_]], Y[_[_]]](fg: F ≈> G, xy: X ≈> Y): ([a[_]] =>> (F[a], X[a])) ≈> ([a[_]] =>> (G[a], Y[a])) =
-      ∀~.mk[([a[_]] =>> (F[a], X[a])) ≈> ([a[_]] =>> (G[a], Y[a]))].fromH([T[_]] => () => Associative[* => *, (*, *)].grouped(fg[T], xy[T]))
-    def fst[F[_[_]], G[_[_]]]: ([a[_]] =>> (F[a], G[a])) ≈> F = ∀~.mk[([a[_]] =>> (F[a], G[a])) ≈> F].from(_._1)
-    def snd[F[_[_]], G[_[_]]]: ([a[_]] =>> (F[a], G[a])) ≈> G = ∀~.mk[([a[_]] =>> (F[a], G[a])) ≈> G].from(_._2)
-    def diag[F[_[_]]]: F ≈> ([a[_]] =>> (F[a], F[a])) = ∀~.mk[F ≈> ([a[_]] =>> (F[a], F[a]))].from(fa => (fa, fa))
-    def merge[F[_[_]], G[_[_]], H[_[_]]](f: F ≈> G, g: F ≈> H): F ≈> ([a[_]] =>> (G[a], H[a])) =
-      ∀~.mk[F ≈> ([a[_]] =>> (G[a], H[a]))].from(fa => (f.apply(fa), g.apply(fa)))
-    def idl[F[_[_]]]: ([a[_]] =>> (UnitHK[a], F[a])) ≈> F = ∀~.mk[([a[_]] =>> (UnitHK[a], F[a])) ≈> F].from(_._2)
-    def idr[F[_[_]]]: ([a[_]] =>> (F[a], UnitHK[a])) ≈> F = ∀~.mk[([a[_]] =>> (F[a], UnitHK[a])) ≈> F].from(_._1)
-    def coidl[F[_[_]]]: F ≈> ([a[_]] =>> (UnitHK[a], F[a])) = ∀~.mk[F ≈> ([a[_]] =>> (UnitHK[a], F[a]))].from(fa => ((), fa))
-    def coidr[F[_[_]]]: F ≈> ([a[_]] =>> (F[a], UnitHK[a])) = ∀~.mk[F ≈> ([a[_]] =>> (F[a], UnitHK[a]))].from(fa => (fa, ()))
-    def braid[F[_[_]], G[_[_]]]: ([a[_]] =>> (F[a], G[a])) ≈> ([a[_]] =>> (G[a], F[a])) =
-      ∀~.mk[([a[_]] =>> (F[a], G[a])) ≈> ([a[_]] =>> (G[a], F[a]))].from(_.swap)
+    def associate[A[_[_]], B[_[_]], H[_[_]]]: ([f[_]] =>> ((A[f], B[f]), H[f])) ≈> ([f[_]] =>> (A[f], (B[f], H[f]))) =
+      ∀~.mk[([f[_]] =>> ((A[f], B[f]), H[f])) ≈> ([f[_]] =>> (A[f], (B[f], H[f])))].from(Associative[* => *, (*, *)].associate)
+    def diassociate[A[_[_]], B[_[_]], H[_[_]]]: ([f[_]] =>> (A[f], (B[f], H[f]))) ≈> ([f[_]] =>> ((A[f], B[f]), H[f])) =
+      ∀~.mk[([f[_]] =>> (A[f], (B[f], H[f]))) ≈> ([f[_]] =>> ((A[f], B[f]), H[f]))].from(Associative[* => *, (*, *)].diassociate)
+    def bimap[A[_[_]], B[_[_]], X[_[_]], Y[_[_]]](ab: A ≈> B, xy: X ≈> Y): ([f[_]] =>> (A[f], X[f])) ≈> ([f[_]] =>> (B[f], Y[f])) =
+      ∀~.mk[([f[_]] =>> (A[f], X[f])) ≈> ([f[_]] =>> (B[f], Y[f]))].fromH([T[_]] => () => Associative[* => *, (*, *)].grouped(ab[T], xy[T]))
+    def fst[A[_[_]], B[_[_]]]: ([f[_]] =>> (A[f], B[f])) ≈> A = ∀~.mk[([f[_]] =>> (A[f], B[f])) ≈> A].from(_._1)
+    def snd[A[_[_]], B[_[_]]]: ([f[_]] =>> (A[f], B[f])) ≈> B = ∀~.mk[([f[_]] =>> (A[f], B[f])) ≈> B].from(_._2)
+    def diag[A[_[_]]]: A ≈> ([f[_]] =>> (A[f], A[f])) = ∀~.mk[A ≈> ([f[_]] =>> (A[f], A[f]))].from(fa => (fa, fa))
+    def merge[A[_[_]], B[_[_]], H[_[_]]](f: A ≈> B, g: A ≈> H): A ≈> ([f[_]] =>> (B[f], H[f])) =
+      ∀~.mk[A ≈> ([f[_]] =>> (B[f], H[f]))].from(fa => (f.apply(fa), g.apply(fa)))
+    def idl[A[_[_]]]: ([f[_]] =>> (UnitHK[f], A[f])) ≈> A = ∀~.mk[([f[_]] =>> (UnitHK[f], A[f])) ≈> A].from(_._2)
+    def idr[A[_[_]]]: ([f[_]] =>> (A[f], UnitHK[f])) ≈> A = ∀~.mk[([f[_]] =>> (A[f], UnitHK[f])) ≈> A].from(_._1)
+    def coidl[A[_[_]]]: A ≈> ([f[_]] =>> (UnitHK[f], A[f])) = ∀~.mk[A ≈> ([f[_]] =>> (UnitHK[f], A[f]))].from(fa => ((), fa))
+    def coidr[A[_[_]]]: A ≈> ([f[_]] =>> (A[f], UnitHK[f])) = ∀~.mk[A ≈> ([f[_]] =>> (A[f], UnitHK[f]))].from(fa => (fa, ()))
+    def braid[A[_[_]], B[_[_]]]: ([f[_]] =>> (A[f], B[f])) ≈> ([f[_]] =>> (B[f], A[f])) =
+      ∀~.mk[([f[_]] =>> (A[f], B[f])) ≈> ([f[_]] =>> (B[f], A[f]))].from(_.swap)
   }
   object coproduct {
-    def diassociate[F[_[_]], G[_[_]], H[_[_]]]: ([a[_]] =>> Either[Either[F[a], G[a]], H[a]]) ≈> ([a[_]] =>> Either[F[a], Either[G[a], H[a]]]) =
-      ∀~.mk[([a[_]] =>> Either[Either[F[a], G[a]], H[a]]) ≈> ([a[_]] =>> Either[F[a], Either[G[a], H[a]]])].from(Associative[* => *, Either].associate)
-    def associate[F[_[_]], G[_[_]], H[_[_]]]: ([a[_]] =>> Either[F[a], Either[G[a], H[a]]]) ≈> ([a[_]] =>> Either[Either[F[a], G[a]], H[a]]) =
-      ∀~.mk[([a[_]] =>> Either[F[a], Either[G[a], H[a]]]) ≈> ([a[_]] =>> Either[Either[F[a], G[a]], H[a]])].from(Associative[* => *, Either].diassociate)
-    def bimap[F[_[_]], G[_[_]], X[_[_]], Y[_[_]]](fg: F ≈> G, xy: X ≈> Y): ([a[_]] =>> Either[F[a], X[a]]) ≈> ([a[_]] =>> Either[G[a], Y[a]]) =
-      ∀~.mk[([a[_]] =>> Either[F[a], X[a]]) ≈> ([a[_]] =>> Either[G[a], Y[a]])].fromH([T[_]] => () => Associative[* => *, Either].grouped(fg[T], xy[T]))
-    def inl[F[_[_]], G[_[_]]]: F ≈> ([a[_]] =>> Either[F[a], G[a]]) = ∀~.mk[F ≈> ([a[_]] =>> Either[F[a], G[a]])].from(_.asLeft)
-    def inr[F[_[_]], G[_[_]]]: G ≈> ([a[_]] =>> Either[F[a], G[a]]) = ∀~.mk[G ≈> ([a[_]] =>> Either[F[a], G[a]])].from(_.asRight)
-    def codiag[F[_[_]]]: ([a[_]] =>> Either[F[a], F[a]]) ≈> F = ∀~.mk[([a[_]] =>> Either[F[a], F[a]]) ≈> F].from(_.fold(identity, identity))
-    def split[F[_[_]], G[_[_]], H[_[_]]](f: F ≈> H, g: G ≈> H): ([a[_]] =>> Either[F[a], G[a]]) ≈> H =
-      ∀~.mk[([a[_]] =>> Either[F[a], G[a]]) ≈> H].from(_.fold(f.apply, g.apply))
-    def coidl[F[_[_]], G[_[_]]]: ([a[_]] =>> Either[VoidHK[a], F[a]]) ≈> F = ∀~.mk[([a[_]] =>> Either[VoidHK[a], F[a]]) ≈> F].from(_.fold(identity, identity))
-    def coidr[F[_[_]], G[_[_]]]: ([a[_]] =>> Either[F[a], VoidHK[a]]) ≈> F = ∀~.mk[([a[_]] =>> Either[F[a], VoidHK[a]]) ≈> F].from(_.fold(identity, identity))
-    def idl[F[_[_]]]: F ≈> ([a[_]] =>> Either[VoidHK[a], F[a]]) = ∀~.mk[F ≈> ([a[_]] =>> Either[VoidHK[a], F[a]])].from(_.asRight)
-    def idr[F[_[_]]]: F ≈> ([a[_]] =>> Either[F[a], VoidHK[a]]) = ∀~.mk[F ≈> ([a[_]] =>> Either[F[a], VoidHK[a]])].from(_.asLeft)
-    def braid[F[_[_]], G[_[_]]]: ([a[_]] =>> Either[F[a], G[a]]) ≈> ([a[_]] =>> Either[G[a], F[a]]) =
-      ∀~.mk[([a[_]] =>> Either[F[a], G[a]]) ≈> ([a[_]] =>> Either[G[a], F[a]])].from(_.swap)
+    def diassociate[A[_[_]], B[_[_]], H[_[_]]]: ([f[_]] =>> Either[Either[A[f], B[f]], H[f]]) ≈> ([f[_]] =>> Either[A[f], Either[B[f], H[f]]]) =
+      ∀~.mk[([f[_]] =>> Either[Either[A[f], B[f]], H[f]]) ≈> ([f[_]] =>> Either[A[f], Either[B[f], H[f]]])].from(Associative[* => *, Either].associate)
+    def associate[A[_[_]], B[_[_]], H[_[_]]]: ([f[_]] =>> Either[A[f], Either[B[f], H[f]]]) ≈> ([f[_]] =>> Either[Either[A[f], B[f]], H[f]]) =
+      ∀~.mk[([f[_]] =>> Either[A[f], Either[B[f], H[f]]]) ≈> ([f[_]] =>> Either[Either[A[f], B[f]], H[f]])].from(Associative[* => *, Either].diassociate)
+    def bimap[A[_[_]], B[_[_]], X[_[_]], Y[_[_]]](ab: A ≈> B, xy: X ≈> Y): ([f[_]] =>> Either[A[f], X[f]]) ≈> ([f[_]] =>> Either[B[f], Y[f]]) =
+      ∀~.mk[([f[_]] =>> Either[A[f], X[f]]) ≈> ([f[_]] =>> Either[B[f], Y[f]])].fromH([T[_]] => () => Associative[* => *, Either].grouped(ab[T], xy[T]))
+    def inl[A[_[_]], B[_[_]]]: A ≈> ([f[_]] =>> Either[A[f], B[f]]) = ∀~.mk[A ≈> ([f[_]] =>> Either[A[f], B[f]])].from(_.asLeft)
+    def inr[A[_[_]], B[_[_]]]: B ≈> ([f[_]] =>> Either[A[f], B[f]]) = ∀~.mk[B ≈> ([f[_]] =>> Either[A[f], B[f]])].from(_.asRight)
+    def codiag[A[_[_]]]: ([f[_]] =>> Either[A[f], A[f]]) ≈> A = ∀~.mk[([f[_]] =>> Either[A[f], A[f]]) ≈> A].from(_.fold(identity, identity))
+    def split[A[_[_]], B[_[_]], H[_[_]]](f: A ≈> H, g: B ≈> H): ([f[_]] =>> Either[A[f], B[f]]) ≈> H =
+      ∀~.mk[([f[_]] =>> Either[A[f], B[f]]) ≈> H].from(_.fold(f.apply, g.apply))
+    def coidl[A[_[_]], B[_[_]]]: ([f[_]] =>> Either[VoidHK[f], A[f]]) ≈> A = ∀~.mk[([f[_]] =>> Either[VoidHK[f], A[f]]) ≈> A].from(_.fold(identity, identity))
+    def coidr[A[_[_]], B[_[_]]]: ([f[_]] =>> Either[A[f], VoidHK[f]]) ≈> A = ∀~.mk[([f[_]] =>> Either[A[f], VoidHK[f]]) ≈> A].from(_.fold(identity, identity))
+    def idl[A[_[_]]]: A ≈> ([f[_]] =>> Either[VoidHK[f], A[f]]) = ∀~.mk[A ≈> ([f[_]] =>> Either[VoidHK[f], A[f]])].from(_.asRight)
+    def idr[A[_[_]]]: A ≈> ([f[_]] =>> Either[A[f], VoidHK[f]]) = ∀~.mk[A ≈> ([f[_]] =>> Either[A[f], VoidHK[f]])].from(_.asLeft)
+    def braid[A[_[_]], B[_[_]]]: ([f[_]] =>> Either[A[f], B[f]]) ≈> ([f[_]] =>> Either[B[f], A[f]]) =
+      ∀~.mk[([f[_]] =>> Either[A[f], B[f]]) ≈> ([f[_]] =>> Either[B[f], A[f]])].from(_.swap)
   }
 }
